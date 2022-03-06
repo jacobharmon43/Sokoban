77892409ba8acf4e30b92a01b111cad7a60f0427
jacobharmon43 <jacobharmon43@gmail.com>
2022-03-05 18:46:31 -0600

    Fixed error in formatting

--- Code/TileBound.cs
+++ Code/TileBound.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 namespace BlockPuzzle{
     public class TileBound : MonoBehaviour
     {
-        
         private Tilemap _groundTiles;
         protected float _delay = 0.1f;
         protected float _timer = 0.0f;
         public Vector3Int _gridPosition;
         private void Start(){
             _gridPosition = _groundTiles.WorldToCell(transform.position);
-            if(!_groundTiles.HasTile(_gridPosition)){
+            if(!_groundTiles.HasTile(_gridPosition))
                 throw new System.Exception("Player spawned off ground tile grid");
-            }
+        }
             
-            public virtual void Move(Vector3Int input){
-                Vector3Int nextPos = _gridPosition + input;
-                if(_groundTiles.HasTile(nextPos)){
-                    _gridPosition = nextPos;
-                    transform.position = _groundTiles.CellToWorld(nextPos);
-                }
+        public virtual void Move(Vector3Int input){
+            Vector3Int nextPos = _gridPosition + input;
+            if(_groundTiles.HasTile(nextPos)){
+                _gridPosition = nextPos;
+                transform.position = _groundTiles.CellToWorld(nextPos);
             }
         }
     }
