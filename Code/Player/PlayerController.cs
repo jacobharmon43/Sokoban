c6d8db1d7d1f635951520c0aaa74fa24aac4a32c
jacobharmon43 <jacobharmon43@gmail.com>
2022-03-06 21:46:52 -0600

    Some tile chances

--- Code/Player/PlayerController.cs
+++ Code/Player/PlayerController.cs
@@ -5,11 +5,10 @@ namespace BlockPuzzle.Player
     [RequireComponent(typeof(PlayerInputReader))]
     public class PlayerController : TileBound
     {
-        private LayerMask _pushableLayer;
         private PlayerInputReader _input;
+
         private void Awake(){
             _input = GetComponent<PlayerInputReader>();
-            _pushableLayer = LayerMask.GetMask("Pushable");
         }
 
         private void Update(){
@@ -32,16 +31,6 @@ namespace BlockPuzzle.Player
                 Slidable s = c.GetComponent<Slidable>();
                 s.Kick(GridPosition - s.GridPosition);
             }
-        }
-
-        public override void Move(Vector3Int input)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(input.x, input.y), 1, _pushableLayer);
-            if(hit){
-                Slidable s = hit.transform.GetComponent<Slidable>();
-                s.Move(input);
-            }
-            base.Move(input);
         }
     }
 }
--- Code/TileBound.cs
+++ Code/TileBound.cs
@@ -5,30 +5,39 @@ namespace BlockPuzzle{
     public class TileBound : MonoBehaviour
     {
         [SerializeField] private Tilemap _groundTiles;
+        protected LayerMask _pushableLayer;
         protected float _delay = 0.1f;
         protected float _timer = 0.0f;
         public Vector3Int GridPosition;
 
         private void Start(){
+            _pushableLayer = LayerMask.GetMask("Pushable");
             GridPosition = _groundTiles.WorldToCell(transform.position);
             if(!_groundTiles.HasTile(GridPosition))
-                throw new System.Exception("Player spawned off ground tile grid");
+                throw new System.Exception($"Tilebound object: {transform.name} starting location is off the grid");
             transform.position = _groundTiles.CellToWorld(GridPosition);
         }
             
-        public virtual void Move(Vector3Int input){
-            Vector3Int nextPos = GridPosition + input;
-            if(_groundTiles.HasTile(nextPos)){
-                Vector3 goTo = _groundTiles.CellToWorld(nextPos);
-                TileBound[] set = GameObject.FindObjectsOfType<Slidable>();
-                foreach(TileBound obj in set){
-                    if(obj.GridPosition == nextPos){
-                        return;
-                    }
-                }
-                GridPosition = nextPos;
-                transform.position = goTo;
+        public virtual bool Move(Vector3Int input){
+            Vector3Int nextPos = GridPosition + input; // Calculate next gridposition
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(input.x, input.y), 1, _pushableLayer);
+            if(hit){
+                Slidable s = hit.transform.GetComponent<Slidable>();
+                s.Move(input);
             }
+            if(!_groundTiles.HasTile(nextPos)){
+                return false;
+            }
+            Vector3 goTo = _groundTiles.CellToWorld(nextPos);
+            TileBound[] set = GameObject.FindObjectsOfType<Slidable>();
+            foreach(TileBound obj in set){
+                if(obj.GridPosition == nextPos){
+                    return false;
+                }
+            }
+            GridPosition = nextPos;
+            transform.position = goTo;
+            return true;
         }
     }
 }
